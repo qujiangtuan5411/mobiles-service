fbffb63b884082c77fa4719b4f0f67a6758c77b4
fangjianyu <fangjianyu123@icloud.com>
2019-06-27 18:45:30 +0800

    新增JVM监控日志打印

--- src/main/java/com/sioo/log/aspect/JvmMonitorAspect.java
+++ src/main/java/com/sioo/log/aspect/JvmMonitorAspect.java
@@ -1,7 +1,21 @@
 package com.sioo.log.aspect;
 
+import com.sioo.log.annotation.JvmMonitor;
+import com.sioo.log.bo.JvmInfo;
+import com.sioo.log.utils.MonitorUtil;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /****
  * @description：JVM监控切点类
@@ -13,4 +27,95 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class JvmMonitorAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(JvmMonitorAspect.class);
+
+    /**
+     * 线程池
+     */
+    private final ExecutorService fixedThreadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1 );
+
+    /** 
+    * @Description: JVM监控点 
+    * @Param:
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
+    @Pointcut("@annotation(com.sioo.log.annotation.JvmMonitor)")
+    public void jvmMonitorAspect(){
+
+    }
+
+    /** 
+    * @Description: JVM信息打印(方法执行之前)
+    * @Param:
+    * @return:
+    * @Author: fanghuaiming
+    * @Date:
+    */
+    @Before("jvmMonitorAspect()")
+    public void doBefore(JoinPoint joinPoint){
+        fixedThreadPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map<String, String> params = getAnnotationParams(joinPoint);
+                    jvmMonitor(Boolean.parseBoolean(params.get("isOpen")), params.get("critical"));
+                }
+                catch (ClassNotFoundException e) {
+                    logger.error("【JVM监控】监控模块出错-异常详细信息", e);
+                }
+            }
+        });
+    }
+
+    /** 
+    * @Description: 启动JVM监控
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
+    private void jvmMonitor(Boolean isOpen, String critical) {
+        if (isOpen) {
+            JvmInfo jvmInfo = MonitorUtil.initJvmInfo(isOpen, critical);
+            logger.info(
+                    "JVM性能监控信息--当前JVM空闲内存:{}; 当前JVM内存总量:{}; JVM试图使用的最大内存:{}; JVM实际可用内存:{}; 阈值:{}; JVM实际可用内存占有率:{}; 是否低于阈值:{};",
+                    jvmInfo.getFreeMemory() / 1024 / 1024 + "M", jvmInfo.getTotalMemory() / 1024 / 1024 + "M",
+                    jvmInfo.getMaxMemory() / 1024 / 1024 + "M", jvmInfo.getActualAvailableMemory() / 1024 / 1024 + "M",
+                    jvmInfo.getCritical(), jvmInfo.getMemmoryPercent(), jvmInfo.getJudgeValue());
+        }
+    }
+
+    /** 
+    * @Description: 获取注解参数
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
+    private static Map<String, String> getAnnotationParams(JoinPoint joinPoint) throws ClassNotFoundException {
+        Map<String, String> params = new HashMap<String, String>();
+        String targetName = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        Object[] arguments = joinPoint.getArgs();
+        Class targetClass = Class.forName(targetName);
+        Method[] methods = targetClass.getMethods();
+        String isOpen = "";
+        String critical = "";
+        for (Method method : methods) {
+            if (method.getName().equals(methodName)) {
+                Class[] clazzs = method.getParameterTypes();
+                if (clazzs.length == arguments.length) {
+                    isOpen = method.getAnnotation(JvmMonitor.class).isOpen();
+                    critical = method.getAnnotation(JvmMonitor.class).critical();
+                    break;
+                }
+            }
+        }
+        params.put("isOpen", isOpen);
+        params.put("critical", critical);
+        return params;
+    }
 }
--- src/main/java/com/sioo/log/utils/MonitorUtil.java
+++ src/main/java/com/sioo/log/utils/MonitorUtil.java
@@ -17,26 +17,29 @@ import java.util.concurrent.ThreadPoolExecutor;
 */
 public class MonitorUtil {
     
-    /**
-     * 初始化jvm信息
-     * @param isOpen
-     * @param critical
-     * @return
-     * @Description: 
-     * @create date 2017年8月14日下午3:33:40
-     */
+    /** 
+    * @Description: 初始化jvm信息 
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date: 6:44 PM 2019/6/27
+    */
     public static JvmInfo initJvmInfo(Boolean isOpen, String critical) {
         JvmInfo jvmInfo = new JvmInfo();
         jvmInfo.setIsOpen(isOpen);
         jvmInfo.setCritical(critical);
         Runtime rt = Runtime.getRuntime();
-        long totalMemorySize = rt.totalMemory(); //相当于当前JVM已使用的内存及freeMemory()的总和
+        //相当于当前JVM已使用的内存及freeMemory()的总和
+        long totalMemorySize = rt.totalMemory();
         jvmInfo.setTotalMemory(totalMemorySize);
-        long maxMemorySize = rt.maxMemory(); //最大分配内存
+        //最大分配内存
+        long maxMemorySize = rt.maxMemory();
         jvmInfo.setMaxMemory(maxMemorySize);
-        long freeMemorySize = rt.freeMemory(); //JVM空闲内存
+        //JVM空闲内存
+        long freeMemorySize = rt.freeMemory();
         jvmInfo.setFreeMemory(freeMemorySize);
-        long actualAvailableMemory = maxMemorySize - totalMemorySize + freeMemorySize; // JVM实际可用内存:并不等于freeMemory(),而应该等于maxMemory()-totalMemory()+freeMemory()
+        // JVM实际可用内存:并不等于freeMemory(),而应该等于maxMemory()-totalMemory()+freeMemory()
+        long actualAvailableMemory = maxMemorySize - totalMemorySize + freeMemorySize;
         jvmInfo.setActualAvailableMemory(actualAvailableMemory);
         String availableMemoryPercent = MonitorUtil.computePercent(actualAvailableMemory, maxMemorySize);
         jvmInfo.setMemmoryPercent(availableMemoryPercent);
@@ -45,14 +48,13 @@ public class MonitorUtil {
         return jvmInfo;
     }
     
-    /**
-     * 获得线程池信息
-     * @param isOpen
-     * @param threadPoolExecutor
-     * @return
-     * @Description: 
-     * @create date 2017年8月14日下午5:36:37
-     */
+    /** 
+    * @Description: 获得线程池信息 
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
     public static ThreadPoolInfo initThreadPoolInfo(Boolean isOpen, ThreadPoolExecutor threadPoolExecutor) {
         ThreadPoolInfo info = new ThreadPoolInfo();
         info.setIsOpen(isOpen);
@@ -65,14 +67,13 @@ public class MonitorUtil {
         return info;
     }
 
-    /**
-     * 获取百分比
-     * @param molecular 分子
-     * @param denominator 分母
-     * @return
-     * @Description: 保留两位小数
-     * @create date 2017年8月11日上午10:52:57
-     */
+    /** 
+    * @Description: 获取百分比 
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
     public static String computePercent(double molecular, double denominator) {
         String str;
         double p = molecular / denominator;
@@ -82,14 +83,13 @@ public class MonitorUtil {
         return str;
     }
 
-    /**
-     * 判断某种情况百分比低于某一界定值的方法
-     * @param usedPercent
-     * @param critical
-     * @return
-     * @Description: 
-     * @create date 2017年8月11日上午11:06:40
-     */
+    /** 
+    * @Description: 判断某种情况百分比低于某一界定值的方法 
+    * @Param:  
+    * @return:  
+    * @Author: fanghuaiming
+    * @Date:  
+    */
     public static int comparePercent(String usedPercent, String critical) {
         String tempA = usedPercent.substring(0, usedPercent.indexOf("%"));
         String tempB = critical.substring(0, critical.indexOf("%"));
