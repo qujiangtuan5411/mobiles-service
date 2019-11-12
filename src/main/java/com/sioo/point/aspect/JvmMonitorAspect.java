3af518029cf9fd3009f3b898b9fc8342840fea0f
fangjianyu <fangjianyu123@icloud.com>
2019-11-12 17:08:55 +0800

    JVM监控报警机器人通知

--- src/main/java/com/sioo/point/aspect/JvmMonitorAspect.java
+++ src/main/java/com/sioo/point/aspect/JvmMonitorAspect.java
@@ -3,15 +3,18 @@ package com.sioo.point.aspect;
 import com.sioo.point.annotation.JvmMonitor;
 import com.sioo.point.bo.JvmInfo;
 import com.sioo.point.utils.MonitorUtil;
+import com.sioo.wechatreboot.invoke.JvmWechatReboot;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,6 +32,9 @@ import java.util.concurrent.Executors;
 @Component
 @Order(3)
 public class JvmMonitorAspect {
+
+    @Autowired
+    private JvmWechatReboot jvmWechatReboot;
 
     /**
      * 日志对象
@@ -67,7 +73,7 @@ public class JvmMonitorAspect {
             public void run() {
                 try {
                     Map<String, String> params = getAnnotationParams(joinPoint);
-                    jvmMonitor(Boolean.parseBoolean(params.get("isOpen")), params.get("critical"));
+                    jvmMonitor(Boolean.parseBoolean(params.get("isOpen")), params.get("critical"),joinPoint.getSignature().getName(),joinPoint.getSignature().getDeclaringTypeName());
                 }
                 catch (ClassNotFoundException e) {
                     logger.error("【JVM监控】监控模块出错-异常详细信息", e);
@@ -83,14 +89,44 @@ public class JvmMonitorAspect {
     * @Author: fanghuaiming
     * @Date:  
     */
-    private void jvmMonitor(Boolean isOpen, String critical) {
+    private void jvmMonitor(Boolean isOpen, String critical,String signatureName,String declaringTypeName) {
         if (isOpen) {
             JvmInfo jvmInfo = MonitorUtil.initJvmInfo(isOpen, critical);
+            //空闲内存
+            long freeMemory = jvmInfo.getFreeMemory() / 1024 / 1024;
+            //总内存
+            long totalMemory = jvmInfo.getTotalMemory() / 1024 / 1024;
+            //最大内存
+            long maxMemory = jvmInfo.getMaxMemory() / 1024 / 1024;
+            //实际可用内存
+            long actualAvailableMemory = jvmInfo.getActualAvailableMemory() / 1024 / 1024;
+            //阀值
+            String critical1 = jvmInfo.getCritical();
+            //可用内存占有率
+            String memmoryPercent = jvmInfo.getMemmoryPercent();
+            //是否低于阀值
+            Boolean judgeValue = jvmInfo.getJudgeValue();
             logger.info(
                     "JVM性能监控信息--当前JVM空闲内存:{}; 当前JVM内存总量:{}; JVM试图使用的最大内存:{}; JVM实际可用内存:{}; 阈值:{}; JVM实际可用内存占有率:{}; 是否低于阈值:{};",
-                    jvmInfo.getFreeMemory() / 1024 / 1024 + "M", jvmInfo.getTotalMemory() / 1024 / 1024 + "M",
-                    jvmInfo.getMaxMemory() / 1024 / 1024 + "M", jvmInfo.getActualAvailableMemory() / 1024 / 1024 + "M",
-                    jvmInfo.getCritical(), jvmInfo.getMemmoryPercent(), jvmInfo.getJudgeValue());
+                    freeMemory + "M", totalMemory + "M",
+                    maxMemory + "M", actualAvailableMemory + "M",
+                    critical1, memmoryPercent, judgeValue);
+            try {
+                jvmWechatReboot.sendJvmMarkdownMessage("JVM性能监控报警信息!!!",
+                        freeMemory,
+                        totalMemory,
+                        maxMemory,
+                        actualAvailableMemory,
+                        critical1,
+                        memmoryPercent,
+                        judgeValue,
+                        signatureName,
+                        declaringTypeName
+                        );
+            } catch (IOException e) {
+                e.printStackTrace();
+                logger.debug("机器人JVM监控报警出现异常,请检查!!!");
+            }
         }
     }
 
