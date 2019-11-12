3f12052c39bf44b08f787564d7de962c0ae95ea8
fangjianyu <fangjianyu123@icloud.com>
2019-11-12 17:36:06 +0800

    添加自动化配置注释

--- src/main/java/com/sioo/point/properties/SlaveRedisProperties.java
+++ src/main/java/com/sioo/point/properties/SlaveRedisProperties.java
@@ -26,22 +26,49 @@ public class SlaveRedisProperties {
     private static final  int DEFAULT_PORT = 6379;
     private static final  String DEFAULT_PASSWORD = "SIOO123!@#";
 
+    /**
+     * 切换使用的库
+     */
     private  int database = DEFAULT_DATABASE;
 
+    /**
+     * 连接超时时间
+     */
     private  long timeout = DEFAULT_TIMEOUT;
 
+    /**
+     * 最大存活时间
+     */
     private  int maxActive = DEFAULT_MAXACTIVE;
 
+    /**
+     * 最大等待的时间
+     */
     private int maxWait = DEFAULT_MAXWAIT;
 
+    /**
+     * 最大连接数
+     */
     private int maxIdle = DEFAULT_MAXIDLE;
 
+    /**
+     * 最小连接数
+     */
     private int minIdle = DEFAULT_MINIDLE;
 
+    /**
+     * 连接host（ip）
+     */
     private String hostName = DEFAULT_HOSTNAME;
 
+    /**
+     * 端口port
+     */
     private int port = DEFAULT_PORT;
 
+    /**
+     * 密码
+     */
     private String password = DEFAULT_PASSWORD;
 
 }
